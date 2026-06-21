3cec19ae861db21191df10329e759038d6380dfb
Michael Scinocca <mscinocca@gmail.com>
2026-06-21 17:03:15 -0400

    feat: Day 10

--- src/days.rs
+++ src/days.rs
@@ -7,6 +7,7 @@ mod day6;
 mod day7;
 mod day8;
 mod day9;
+mod day10;
 
 pub fn run_day_part(day: i32, part: i32) {
     match (day, part) {
@@ -28,6 +29,8 @@ pub fn run_day_part(day: i32, part: i32) {
         (8, 2) => day8::part2(),
         (9, 1) => day9::part1(),
         (9, 2) => day9::part2(),
+        (10, 1) => day10::part1(),
+        (10, 2) => day10::part2(),
         _ => (),
     }
 }
---
+++ src/days/day10.rs
@@ -0,0 +1,83 @@
+pub fn part1() {
+    let mut input = "1113122113";
+
+    let mut output = String::new();
+    
+    for _ in 0..40 {
+        output = transform(input);
+        input = output.as_str();
+    }
+
+    println!("{}", output.len());
+}
+
+pub fn part2() {
+    let mut input = "1113122113";
+
+    let mut output = String::new();
+    
+    for _ in 0..50 {
+        output = transform(input);
+        input = output.as_str();
+    }
+
+    println!("{}", output.len());
+}
+
+fn transform(input: &str) -> String {
+    let mut output = String::new();
+
+    let mut count = 0;
+    let mut current_digit = ' ';
+    
+    for digit in input.chars() {
+        if current_digit == ' ' {
+            current_digit = digit;
+        }
+
+        if digit == current_digit {
+            count += 1;
+        } else {
+            output.push_str(&count.to_string());
+            output.push(current_digit);
+
+            count = 1;
+            current_digit = digit;
+        }
+    }
+
+    output.push_str(&count.to_string());
+    output.push(current_digit);
+    
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(transform("1"), "11");
+    }
+
+    #[test]
+    fn test_11() {
+        assert_eq!(transform("11"), "21");
+    }
+
+    #[test]
+    fn test_21() {
+        assert_eq!(transform("21"), "1211");
+    }
+
+    #[test]
+    fn test_1211() {
+        assert_eq!(transform("1211"), "111221");
+    } 
+
+    #[test]
+    fn test_111221() {
+        assert_eq!(transform("111221"), "312211");
+    } 
+}
